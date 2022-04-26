8de566990af3130c619bd1fe42ff11c368e008cf
김희망 <kimhm84@brandi.co.kr>
2022-04-26 12:41:59 +0900

    feature: public/protect 엑셀파일 다운로드 API 추가

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const {createFile, createStream} = require("../service/excelService");
-const stream = require("stream");
 const {BadRequestError} = require("../model/badRequestError");
 
 /**
